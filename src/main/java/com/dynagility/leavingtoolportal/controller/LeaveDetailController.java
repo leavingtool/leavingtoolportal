4c8b10a5676fc70e715887e8b19bef59243e72b9
linh <ninhnguyentma17@gmail.com>
2017-05-16 15:42:32 +0700

    Rebuild for Viet

--- src/main/java/com/dynagility/leavingtoolportal/controller/LeaveDetailController.java
+++ src/main/java/com/dynagility/leavingtoolportal/controller/LeaveDetailController.java
@@ -1,7 +1,5 @@
 package com.dynagility.leavingtoolportal.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dynagility.leavingtoolportal.VO.DateTimeLeaveVO;
-import com.dynagility.leavingtoolportal.VO.EmployeeVO;
 import com.dynagility.leavingtoolportal.VO.LeaveDetailVO;
 import com.dynagility.leavingtoolportal.service.LeaveDetailService;
 import com.dynagility.leavingtoolportal.service.LeaveReasonByYearService;
